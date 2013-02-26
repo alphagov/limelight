995c3f063e02119ff3102b68470e8222bf7c9f25
Oliver Spindler <os@oliverspindler.com>
2013-02-26 12:21:05 +0000

    parse timestamp model attributes into moment dates

--- app/assets/javascripts/extensions/model.js
+++ app/assets/javascripts/extensions/model.js
@@ -1,9 +1,24 @@
 define([
-    'backbone'
+    'backbone',
+    'moment'
 ],
-function (Backbone) {
+function (Backbone, moment) {
     var Model = Backbone.Model.extend({
-        customProperty: true
+        customProperty: true,
+        
+        parse: function (attributes) {
+            attributes = Backbone.Model.prototype.parse(attributes);
+            if (attributes['_timestamp']) {
+                var timestamp = moment(attributes['_timestamp']);
+                if (timestamp.isValid()) {
+                    attributes['_timestamp'] = timestamp;
+                } else {
+                    console.warn(attributes['_timestamp'], 'is not a valid date');
+                }
+            }
+            
+            return attributes;
+        }
     });
     
     return Model;
--- app/assets/javascripts/spec/extensions/spec.model.js
+++ app/assets/javascripts/spec/extensions/spec.model.js
@@ -13,5 +13,34 @@ function (Model, Backbone) {
             var model = new Model();
             expect(model.customProperty).toBe(true);
         });
+        
+        describe("parse", function() {
+            
+            it("converts valid timestamps into moment date objects", function() {
+                var model = new Model({
+                    foo: 'bar',
+                    '_timestamp': '2013'
+                }, {
+                    parse: true
+                });
+                
+                expect(moment.isMoment(model.get('_timestamp'))).toBe(true);
+                expect(model.get('_timestamp').format('YYYY-MM-DD')).toEqual('2013-01-01');
+            });
+            
+            it("warns when a timestamp is invalid", function() {
+                spyOn(window.console, "warn");
+                var model = new Model({
+                    foo: 'bar',
+                    '_timestamp': 'asdf'
+                }, {
+                    parse: true
+                });
+                
+                expect(moment.isMoment(model.get('_timestamp'))).toBe(false);
+                expect(console.warn).toHaveBeenCalled();
+            });
+            
+        });
     });
 });
