a34b6f892d14da728371821f2d7c12ee8c3c14f1
Pawel Badenski <Pawel.Badenski@thoughtworks.com>
2013-03-01 11:17:23 +0000

    Remove duplication of require config in dev and test
    @abersager
    @pbadenski

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,4 +31,6 @@ Limelight::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.assets.paths << Rails.root.join("spec", "javascripts")
 end
