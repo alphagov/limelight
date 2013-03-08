0e6b01c516be4019b32e5b3df60e21dd7323c30e
Oliver Spindler <os@oliverspindler.com>
2013-03-08 16:40:29 +0000

    let models auto parse start_at and end_at attributes

--- spec/javascripts/spec/extensions/spec.model.js
+++ spec/javascripts/spec/extensions/spec.model.js
@@ -9,23 +9,23 @@ function (Model, Backbone) {
             expect(model instanceof Backbone.Model).toBe(true);
         });
 
-        it("defines a custom property", function() {
-            var model = new Model();
-            expect(model.customProperty).toBe(true);
-        });
-        
         describe("parse", function() {
             
-            it("converts valid timestamps into moment date objects", function() {
+            it("converts valid date attributes into moment date objects", function() {
                 var model = new Model({
                     foo: 'bar',
-                    '_timestamp': '2013-01-01'
+                    '_timestamp': '2013-01-01T00:00:00+00:00',
+                    '_start_at': '2013-01-02T00:00:00+00:00',
+                    '_end_at': '2013-01-03T00:00:00+00:00'
                 }, {
                     parse: true
                 });
                 
+                expect(model.get('foo')).toEqual('bar');
                 expect(moment.isMoment(model.get('_timestamp'))).toBe(true);
                 expect(model.get('_timestamp').format('YYYY-MM-DD')).toEqual('2013-01-01');
+                expect(model.get('_start_at').format('YYYY-MM-DD')).toEqual('2013-01-02');
+                expect(model.get('_end_at').format('YYYY-MM-DD')).toEqual('2013-01-03');
             });
             
             it("warns when a timestamp is invalid", function() {
