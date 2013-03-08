9136154733de1ca4ac953604faf25283ebdf7506
Oliver Spindler <os@oliverspindler.com>
2013-03-08 13:45:27 +0000

    hide global gov.uk navigation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  # use 'homepage' template to remove global navigation element
+  include Slimmer::Template
+  slimmer_template :homepage
 end
