c49d49c7bab8b30098cc74716539c37ae0b21ab5
Oliver Spindler <os@oliverspindler.com>
2013-03-15 16:12:22 +0000

    add MultiCollection extension

---
+++ app/assets/javascripts/extensions/multicollection.js
@@ -0,0 +1,81 @@
+define([
+  'extensions/collection'
+],
+function (Collection) {
+  
+  /**
+   * Combines data from multiple Backdrop queries
+   */
+  var MultiCollection = Collection.extend({
+    
+    /**
+     * Defines the constituent collection classes.
+     * Override in subclass or options
+     */
+    collections: [],
+    
+    initialize: function (models, options) {
+      if (options && options.collections) {
+        this.collections = options.collections;
+      }
+      this.collectionInstances = _.map(this.collections, function (classRef) {
+        return new classRef(models, options);
+      });
+    },
+    
+    /**
+     * Called when all collections are ready. Override this to calculate
+     * the actual data from the constituent collections.
+     */
+    parse: function (options) {
+      throw("No parser defined for MultiCollection");
+    },
+    
+    /**
+     * Fetches data for all constituent collections. Parses data when all
+     * requests have returned successfully. Fails if any of the requests fail.
+     */
+    fetch: function (options) {
+      options = options || {};
+      
+      var openRequests = numRequests = this.collectionInstances.length;
+      var successfulRequests = 0;
+      var that = this;
+      
+      var onResponse = function () {
+        if (--openRequests > 0) {
+          // wait for other requests to return
+          return;
+        }
+        
+        if (successfulRequests == numRequests) {
+          // all constituent collections returned successfully
+          that.reset.call(that, that.parse.call(that, options));
+        }
+      };
+      var onSuccess = function () {
+        successfulRequests++;
+        onResponse();
+      };
+      
+      _.each(this.collectionInstances, function (collection) {
+        
+        collection.on('error', function () {
+          // escalate error status
+          if (options.error) {
+            options.error.apply(collection, arguments)
+          }
+          var args = ['error'].concat(Array.prototype.slice.call(arguments));
+          this.trigger.apply(this, args);
+        }, this),
+        collection.fetch({
+          success: onSuccess,
+          error: onResponse
+        });
+      }, this);
+    }
+    
+  });
+  
+  return MultiCollection;
+});
---
+++ spec/javascripts/spec/extensions/spec.multicollection.js
@@ -0,0 +1,89 @@
+define([
+  'extensions/multicollection'
+],
+function (MultiCollection) {
+  
+  describe("MultiCollection", function() {
+    
+    describe("initialize", function() {
+      it("creates instances of constituent collections", function() {
+        var part1 = jasmine.createSpy();
+        var part2 = jasmine.createSpy();
+        var collection = new MultiCollection([], {
+          collections: [part1, part2]
+        });
+        expect(part1).toHaveBeenCalled();
+        expect(part2).toHaveBeenCalled();
+        expect(collection.collectionInstances[0] instanceof part1).toBe(true);
+        expect(collection.collectionInstances[1] instanceof part2).toBe(true);
+      });
+    });
+    
+    describe("fetch", function() {
+      var collection, part1, part2;
+      beforeEach(function() {
+        collection = new MultiCollection();
+        part1 = {
+          on: jasmine.createSpy(),
+          fetch: jasmine.createSpy()
+        };
+        part2 = {
+          on: jasmine.createSpy(),
+          fetch: jasmine.createSpy()
+        };
+        collection.collectionInstances = [part1, part2];
+        spyOn(collection, "parse");
+      });
+      
+      it("fetches data for all collections", function() {
+        collection.fetch();
+        expect(part1.fetch).toHaveBeenCalled();
+        expect(part2.fetch).toHaveBeenCalled();
+      });
+      
+      it("parses data once all collections have fetched successfully", function() {
+        collection.fetch();
+        expect(part1.fetch).toHaveBeenCalled();
+        expect(part2.fetch).toHaveBeenCalled();
+        expect(collection.parse).not.toHaveBeenCalled();
+        
+        part2.fetch.argsForCall[0][0].success();
+        expect(collection.parse).not.toHaveBeenCalled();
+        
+        part1.fetch.argsForCall[0][0].success();
+        expect(collection.parse).toHaveBeenCalled();
+      });
+      
+      it("fails as soon as a request fails", function() {
+        var onError1 = jasmine.createSpy();
+        var onError2 = jasmine.createSpy();
+        
+        collection.fetch({
+          error: onError1
+        });
+        collection.on('error', onError2);
+        
+        expect(part2.on.argsForCall[0][0]).toEqual('error');
+        var onErrorListener = part2.on.argsForCall[0][1];
+        
+        expect(part1.fetch).toHaveBeenCalled();
+        expect(part2.fetch).toHaveBeenCalled();
+        expect(onError1).not.toHaveBeenCalled();
+        expect(onError2).not.toHaveBeenCalled();
+        
+        // part 2 fails - the error is escalated immediately
+        onErrorListener.call(collection);
+        expect(collection.parse).not.toHaveBeenCalled();
+        expect(onError1).toHaveBeenCalled();
+        expect(onError2).toHaveBeenCalled();
+        
+        // part 1 returns successfully - parse is still not called
+        part1.fetch.argsForCall[0][0].success();
+        expect(collection.parse).not.toHaveBeenCalled();
+      });
+      
+    });
+    
+  });
+  
+});
