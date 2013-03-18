41dfae637b48bed88f26d20c1119405faebc79c1
Oliver Spindler <os@oliverspindler.com>
2013-03-18 16:18:49 +0000

    allow dynamic graph component configurations

--- app/assets/javascripts/licensing/views/applicationsgraph.js
+++ app/assets/javascripts/licensing/views/applicationsgraph.js
@@ -16,51 +16,53 @@ function (Graph, Axis, Line) {
       right: 40
     },
     
-    components: [
-      {
-        view: Axis,
-        options: {
-          classed: 'x-axis',
-          position: 'bottom',
-          orient: 'bottom',
-          tickPadding: 4,
-          tickFormat: function () {
-            return d3.time.format("%e %b");
-          },
-          offsetY: 20,
-          getScale: function () {
-            return this.scales.x;
+    components: function () {
+      return [
+        {
+          view: Axis,
+          options: {
+            classed: 'x-axis',
+            position: 'bottom',
+            orient: 'bottom',
+            tickPadding: 4,
+            tickFormat: function () {
+              return d3.time.format("%e %b");
+            },
+            offsetY: 20,
+            getScale: function () {
+              return this.scales.x;
+            }
+          }
+        },
+        {
+          view: Axis,
+          options: {
+            position: 'left',
+            classed: 'y-axis',
+            ticks: 7,
+            orient: 'left',
+            getScale: function () {
+              return this.scales.y;
+            },
+            tickFormat: function () {
+              return this.numberListFormatter([this.scales.y.domain()[1]]);
+            }
+          }
+        },
+        {
+          view: Line,
+          options: {
+            classed: 'line',
+            x: function (model) {
+              return this.scales.x(model.get('_start_at').toDate());
+            },
+            y: function (model) {
+              return this.scales.y(model.get('_count'));
+            }
           }
         }
-      },
-      {
-        view: Axis,
-        options: {
-          position: 'left',
-          classed: 'y-axis',
-          ticks: 7,
-          orient: 'left',
-          getScale: function () {
-            return this.scales.y;
-          },
-          tickFormat: function () {
-            return this.numberListFormatter([this.scales.y.domain()[1]]);
-          }
-        }
-      },
-      {
-        view: Line,
-        options: {
-          classed: 'line',
-          x: function (model) {
-            return this.scales.x(model.get('_start_at').toDate());
-          },
-          y: function (model) {
-            return this.scales.y(model.get('_count'));
-          }
-        }
-      }
-    ],
+      ]
+    },
     
     calcXScale: function () {
       var collection = this.collection;
