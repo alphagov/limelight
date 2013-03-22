9a376335bb4849ec8330cb6da88137f81b5c874e
Max Fliri <mfliri@thoughtworks.com>
2013-03-22 15:45:06 +0000

    Create links to licences using rails helpers
    @maxfliri

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Limelight::Application.routes.draw do
   match "/_healthcheck" => "healthcheck#index", via: :get
-  match "/performance/licensing/licences/:slug" => "licensing#per_licence", via: :get
+  match "/performance/licensing/licences/:slug" => "licensing#per_licence", via: :get, as: "licence"
   match "/performance/licensing/licences" => "licensing#licences", via: :get
   match "/performance/licensing" => "licensing#index", via: :get
   
