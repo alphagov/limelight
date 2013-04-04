30fbef97bf686179037df24689990769229b2dd3
Oliver Spindler <os@oliverspindler.com>
2013-04-04 17:17:54 +0100

    make top 5 table not a subclass of applications table

--- app/assets/javascripts/licensing/views/top5table.js
+++ app/assets/javascripts/licensing/views/top5table.js
@@ -1,26 +1,41 @@
 define([
-  'require',
-  './applicationstable'
+  'extensions/table/table'
 ],
-function (require, ApplicationsTable) {
-  var Top5Table = ApplicationsTable.extend({
+function (Table) {
+  var Top5Table = Table.extend({
     
     lazyRender: false,
     
-    initialize: function (options) {
-      options = options || {};
-      
-      this.title = options.title || 'Licence';
-      
-      this.columns[0].title = function () {
-        return this.title;
-      };
-      
-      _.each(this.columns, function (column) {
-        column.sortable = false;
-      });
-      ApplicationsTable.prototype.initialize.apply(this, arguments);
-    }
+    columns: [
+      {
+        id: 'group',
+        title: function () {
+          return this.options.title || 'Licence';
+        },
+        sortable: false,
+        getValue: function (model) {
+          var res = [];
+          _.each(['licenceName', 'authorityName'], function (attr) {
+            var name = model.get(attr);
+            if (name) {
+              res.push(name);
+            }
+          });
+          return res.join(' &ndash; ');
+        }
+      },
+      {
+        id: '_count',
+        title: 'Licence applications',
+        sortable: false,
+        defaultDescending: true,
+        getValue: function (model) {
+          return this.formatNumericLabel(model.get('_count'));
+        }
+      }
+    ],
+    
+    defaultSortColumn: 1
   });
   
   return Top5Table;
