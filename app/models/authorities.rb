6f184d3ef2a9e9a71753938bfeb651531f82a239
Oliver Spindler <os@oliverspindler.com>
2013-04-10 14:45:12 +0100

    Use more idiomatic Ruby approach
    @abersager
    @pbadenski

--- app/models/authorities.rb
+++ app/models/authorities.rb
@@ -1,11 +1,7 @@
 class Authorities
   def self.from_backdrop_response(backdrop_response)
-    authorities_to_return = []
-    unless backdrop_response["data"].empty?
-      backdrop_response["data"].each do |authority_data|
-        authorities_to_return.append(Authority.create(authority_data))
-      end
-    end
-    authorities_to_return
+    backdrop_response["data"].map {|authority_data|
+      Authority.create(authority_data)
+    }
   end
 end
