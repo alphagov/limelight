9518199fa86c02ea9d8c7a944c240d565325cc7d
Pawel Badenski <Pawel.Badenski@thoughtworks.com>
2013-04-17 13:02:56 +0100

    Use standard healthcheck route
    @pbadenski

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Limelight::Application.routes.draw do
-  match "/_healthcheck" => "healthcheck#index", via: :get
+  match "/_status" => "healthcheck#index", via: :get
   match "/performance/licensing/licences/:slug" => "licensing#per_licence", via: :get, as: "licence"
   match "/performance/licensing/authorities/:slug" => "licensing#per_authority", via: :get, as: "authority"
   match "/performance/licensing/licences" => "licensing#licences", via: :get
