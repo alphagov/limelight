1d96fe86f660bd81905551cd7358c57f0cf0e23c
Gareth <grogers@thoughtworks.com>
2013-04-24 17:43:07 +0100

    Reference production.js when running in prod
    @maxfliri
    @gtrogers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     asset_path("config.js").gsub(%r{/[^./]+.js}, "")
   end
 
+  def main_js_path
+    if Rails.env.prod?
+      asset_path("production.js")
+    else
+      asset_path("config.js")
+    end
+  end
+
 end
