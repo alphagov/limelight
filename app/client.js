7b7d862c8d6b2d17b929376ee98fc7ae2b0c586e
Oliver Spindler <os@oliverspindler.com>
2013-04-30 21:28:43 +0100

    minor refactor

--- app/client.js
+++ app/client.js
@@ -2,6 +2,27 @@ require(['config'], function (requireConfig) {
   requireConfig.baseUrl = GOVUK.config.baseUrl || '/limelight';
   require.config(requireConfig);
 
-  require(['bootstrap']);
-})
+  if (window.jasmine) {
+    return;
+  }
+  
+  var backdropUrl = GOVUK.config.backdropUrl || "";
+  var useFakeApi = backdropUrl.indexOf('//fakeapi') !== -1;
+  
+  var loadApplication = function () {
+    var controller = GOVUK.config.controller;
+    require([controller], function (controller) {
+      var view = controller({
+        el: $('#wrapper')
+      });
+      view.render();
+    });
+  };
+
+  if (useFakeApi) {
+    require(['fakeapi'], loadApplication);
+  } else {
+    loadApplication();
+  }
+});
 
