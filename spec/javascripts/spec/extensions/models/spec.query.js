27a755270153871d472e12fe2144481266dfed1b
Oliver Spindler <os@oliverspindler.com>
2013-05-20 10:51:34 +0100

    add 'duration' attribute to query model

--- spec/javascripts/spec/extensions/models/spec.query.js
+++ spec/javascripts/spec/extensions/models/spec.query.js
@@ -16,6 +16,7 @@ function (Query) {
         });
         expect(query.get('foo')).toEqual('bar');
         expect(query.get('period')).toEqual('month');
+        expect(query.get('duration')).toEqual(12);
         expect(query.get('end_at').format('YYYY-MM-DD')).toEqual('2013-05-01');
         expect(query.get('start_at').format('YYYY-MM-DD')).toEqual('2012-05-01');
       });
@@ -31,6 +32,7 @@ function (Query) {
         expect(query.get('a')).toEqual('b');
         expect(query.get('foo')).toEqual('bar');
         expect(query.get('period')).toEqual('month');
+        expect(query.get('duration')).toEqual(12);
         expect(query.get('end_at').format('YYYY-MM-DD')).toEqual('2013-05-01');
         expect(query.get('start_at').format('YYYY-MM-DD')).toEqual('2012-05-01');
       });
@@ -42,6 +44,7 @@ function (Query) {
         query.set('period', 'month');
         expect(query.get('foo')).toEqual('bar');
         expect(query.get('period')).toEqual('month');
+        expect(query.get('duration')).toEqual(12);
         expect(query.get('end_at').format('YYYY-MM-DD')).toEqual('2013-05-01');
         expect(query.get('start_at').format('YYYY-MM-DD')).toEqual('2012-05-01');
       });
@@ -49,6 +52,7 @@ function (Query) {
       it("sets start and end date for 'week' period", function () {
         var query = new Query({ period: 'week' });
         expect(query.get('period')).toEqual('week');
+        expect(query.get('duration')).toEqual(9);
         expect(query.get('end_at').format('YYYY-MM-DD')).toEqual('2013-05-13');
         expect(query.get('start_at').format('YYYY-MM-DD')).toEqual('2013-03-11');
       });
