b78e15bebd88072a05e82ab99b475d3a606d09e6
Gareth <grogers@thoughtworks.com>
2013-05-31 11:04:42 +0100

    Fix test for browser warning preprocessor

--- spec/javascripts/spec/licensing/preprocessors/spec.partial_experience_warning.js
+++ spec/javascripts/spec/licensing/preprocessors/spec.partial_experience_warning.js
@@ -8,7 +8,7 @@ function (preprocessor) {
     it ("blah", function () {
       $('body').append('<div id="global-browser-prompt"><p>Foo</p></div>');
       preprocessor();
-      expect($('#global-browser-prompt').html()).toBe('??');
+      expect($('#global-browser-prompt').html()).toBe('<p>Your browser is unable to display some of the content on this page.</p><p>Foo</p>');
     });
   });
 });
