566111899cb376e9272c6df6691e1f105b346446
Pawel Badenski <Pawel.Badenski@thoughtworks.com>
2013-06-18 14:27:35 +0100

    Load override feature toggles if present
    - Extract a class for loading feature toggles.
    - Load an override feature toggles file if present.
    
    This allows us to override feature toggles on a per-environment basis.
    
    @pbadenski
    @robyoung

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,37 +1,18 @@
 module Settings
   def self.feature_toggles
-    @feature_toggles ||= load_feature_toggles
+    @feature_toggles ||= load_feature_toggles.to_hash
   end
 
   private
-
-  def self.true?(str)
-    regex = /^(true|yes|on|1)$/i
-    regex.match(str).present?
-  end
-
   def self.load_feature_toggles
-    init_feature_toggles(load_yaml("feature_toggles.yml"))
-  end
+    main_yaml_path = Rails.root.join("config", "environments", "feature_toggles.yml")
+    override_yaml_path  = Rails.root.join("config", "environments", "feature_toggles_override.yml")
 
-  def self.init_feature_toggles(yaml)
-    yaml.reduce({}) do |feature_toggles, (feature, activation)|
-      feature_toggles.tap {|ft| ft[feature.to_sym] = toggle_value(activation)}
-    end
-  end
+    toggles = FeatureToggles.new
+    toggles.update_from_yaml_file(main_yaml_path)
+    toggles.update_from_yaml_file(override_yaml_path) if File.exists? override_yaml_path
 
-  def self.toggle_value(activation)
-    case activation
-      when "release"
-        true
-      when "dev"
-        (Rails.env.development? or Rails.env.test?)
-      else
-        false
-    end
-  end
 
-  def self.load_yaml(file_name)
-    YAML.load(File.read(Rails.root.join("config", "environments", file_name)))
+    toggles
   end
 end
