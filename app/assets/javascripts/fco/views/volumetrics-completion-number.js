f0948020f5f63f30d341ddc6398002706faa2abb
Max Fliri <mfliri@thoughtworks.com>
2013-06-28 16:29:06 +0100

    Show mean completion rate for weeks with actual data
    @maxfliri

--- app/assets/javascripts/fco/views/volumetrics-completion-number.js
+++ app/assets/javascripts/fco/views/volumetrics-completion-number.js
@@ -2,6 +2,26 @@ define([
   'extensions/views/view'
 ],
 function (View) {
+  var modelsWith = function(property) {
+    return function(model) {
+      return !_.isUndefined(model.get(property));
+    }
+  }
+
+  var get = function(property) {
+    return function(model) {
+      return model.get(property);
+    }
+  }
+
+  var sum = function(a, b) {
+    return a + b;
+  }
+
+  var contentString = function(value, caption) {
+    return [ '<strong>', value, '%</strong>', caption ].join('');
+  }
+
   var CompletionSelectedView = View.extend({
     initialize: function (attrs, options) {
       View.prototype.initialize.apply(this, arguments);
@@ -10,37 +30,31 @@ function (View) {
 
     render: function () {
       View.prototype.render.apply(this, arguments);
+      this.$el.html(this.content());
+    },
+
+    content:function () {
       var selection = this.collection.getCurrentSelection();
-      var content;
+
       if (selection.selectedModel) {
         var model = selection.selectedModel;
         var start = model.get('_start_at');
         var end = moment(model.get('_end_at')).subtract(1, 'days');
 
-        var percentage = Math.round(selection.selectedModel.get('completion') * 100); 
+        var percentage = Math.round(selection.selectedModel.get('completion') * 100);
 
-        content = [
-          '<strong>',
-          percentage,
-          '%</strong>',
-          start.format(start.month() === end.month() ? 'D' : 'D MMM'),
-          ' – ',
-          end.format('D MMM YYYY')
-        ].join('');
+        var startLabel = start.format(start.month() === end.month() ? 'D' : 'D MMM');
+        var endLabel = end.format('D MMM YYYY');
+        return contentString(percentage, startLabel + '-' + endLabel);
+
       } else {
         var values = this.collection.at(0).get('values');
-        var sum = values.reduce(function (memo, model) {
-          return memo + model.get('completion');
-        }, 0);
-        var percentage = Math.round(100 * sum / values.length);
-        content = [
-          '<strong>',
-          percentage,
-          '%</strong>',
-          ' mean over the last 9 weeks'
-        ].join('');
+        var availableWeeks = values.filter(modelsWith('_id'));
+        var total = availableWeeks.map(get('completion')).reduce(sum);
+        var percentage = Math.round(100 * total / availableWeeks.length);
+
+        return contentString(percentage, ' mean over the last ' + availableWeeks.length + ' weeks');
       }
-      this.$el.html(content);
     }
   });
 
