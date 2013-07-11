29ebd1b5c78b08c460c3b0f4109360e3b7bd6516
Max Fliri <mfliri@thoughtworks.com>
2013-07-11 11:02:12 +0100

    Controller returns a function
    @gtrogers, @maxfliri

--- app/assets/javascripts/lpa/controllers/overview.js
+++ app/assets/javascripts/lpa/controllers/overview.js
@@ -4,20 +4,25 @@ define([
 ], function (
     AvailabilityFor24Hours,
     SingleStatView) {
-
-  var availabilityCollection = new AvailabilityFor24Hours(null, {
-    serviceName: "lasting-power-of-attorney",
-    checkName: "lpa"
-  });
-  var uptimeView = new SingleStatView({
-    collection: availabilityCollection,
-    el: $('#uptime'),
-    getStatFunction: function (collection) { return Math.round(collection.getPercentageOfUptime()) + '%'; }
-  });
-  var responseTimeView = new SingleStatView({
-    collection: availabilityCollection,
-    el: $('#response-time'),
-    getStatFunction: function (collection) { return Math.round(collection.getAverageResponseTime()) + 'ms'; }
-  });
-  availabilityCollection.fetch();
+  return function () {
+    var availabilityCollection = new AvailabilityFor24Hours(null, {
+      serviceName:"lasting-power-of-attorney",
+      checkName:"lpa"
+    });
+    var uptimeView = new SingleStatView({
+      collection:availabilityCollection,
+      el:$('#uptime'),
+      getStatFunction:function (collection) {
+        return Math.round(collection.getPercentageOfUptime()) + '%';
+      }
+    });
+    var responseTimeView = new SingleStatView({
+      collection:availabilityCollection,
+      el:$('#response-time'),
+      getStatFunction:function (collection) {
+        return Math.round(collection.getAverageResponseTime()) + 'ms';
+      }
+    });
+    availabilityCollection.fetch();
+  }
 });
