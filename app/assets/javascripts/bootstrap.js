d8c12f306527f90b4f45cf901d64d973c499ab94
Max Fliri <mfliri@thoughtworks.com>
2013-07-12 11:47:01 +0100

    Fail if a module loaded at bootstrap doesn't return a function
    @gtrogers, @maxfliri

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -1,19 +1,22 @@
 define([ 'require', 'jquery' ], function (require, $) {
   var controller = $('#wrapper').data('controller'),
       preProcessors = $('#wrapper').data('preprocessors'),
-      loadThese = [];
+      modules = [];
 
   if (preProcessors) {
     $.each(preProcessors.split(','), function (i, preProcessor) {
-      loadThese.push("licensing/controllers/preprocessors/" + preProcessor);
+      modules.push("licensing/controllers/preprocessors/" + preProcessor);
     })
   }
 
-  loadThese.push(controller);
+  modules.push(controller);
 
-  require(loadThese, function () {
+  require(modules, function () {
     var args = Array.prototype.slice.call(arguments);
     $.each(args, function (i, func) {
+      if (!_.isFunction(func)) {
+        throw "Module " + modules[i] + " does not return a function";
+      }
       func();
     });
   });
