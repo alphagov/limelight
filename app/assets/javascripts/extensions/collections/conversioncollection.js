9fd5b2bebf003e8712db4adba706a2f75b95cf8a
Gareth <grogers@thoughtworks.com>
2013-07-15 16:52:27 +0100

    Revert "Change conversion funnel to use Mon -> Sun weeks"
    Pushed commit to wrong branch -_-
    
    This reverts commit 667d1d6168c5e827629d4b0b4ffd4bfc2ec60ead.

--- app/assets/javascripts/extensions/collections/conversioncollection.js
+++ app/assets/javascripts/extensions/collections/conversioncollection.js
@@ -5,11 +5,7 @@ define([
 
     queryParams: function() {
       var weeksAgo = this.options.weeksAgo || 0;
-      var today = this.moment();
-      if (today.day() === 0) {
-        weeksAgo += 1;
-      }
-      var startOfWeek = today.day(1).startOf('day').subtract(weeksAgo, 'weeks');
+      var startOfWeek = this.moment().day(1).startOf('day').subtract(weeksAgo, 'weeks');
       
       return {
         start_at: startOfWeek.clone().subtract(1, 'weeks'),
