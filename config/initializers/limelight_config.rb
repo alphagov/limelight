ce58de251c7bc778b221194b4a6c405ce5f63fd2
Gareth <grogers@thoughtworks.com>
2013-07-31 10:48:29 +0100

    Initialise services and feature toggles together.
    @gtrogers, @phss

--- config/initializers/limelight_config.rb
+++ config/initializers/limelight_config.rb
@@ -1,10 +1,17 @@
 require_relative '../../lib/feature_toggles'
+require_relative '../../lib/services'
 
 main_yaml_path = Rails.root.join("config", "feature_toggles.yml")
 override_yaml_path  = Rails.root.join("config", "feature_toggles_override.yml")
+services_yaml_path = Rails.root.join("config", "services.yml")
+
 
 toggles = FeatureToggles.new
 toggles.update_from_yaml_file(main_yaml_path)
 toggles.update_from_yaml_file(override_yaml_path) if File.exists? override_yaml_path
 
 Limelight::Application.config.feature_toggles = toggles.to_hash
+
+services = Services.new(YAML.load(File.read(services_yaml_path)), toggles.to_hash)
+
+Limelight::Application.config.available_services = services.available_services
--- lib/services.rb
+++ lib/services.rb
@@ -7,10 +7,6 @@ class Service
     @excluded_from_list = service_config["excluded_from_list"] || false
     @toggle = service_config["feature_toggle"]
   end
-
-  def toggled_on?
-    @toggle.nil? ? true : Rails.application.config.feature_toggles[@toggle.to_sym]
-  end
 end
 
 class Services
@@ -26,13 +22,3 @@ class Services
     end.index_by(&:slug)
   end
 end
-
-services_yaml_path = Rails.root.join("config", "services.yml")
-
-
-
-dashboards = YAML.load(File.read(services_yaml_path))["dashboards"]
-
-services = dashboards.map {|properties| Service.new(properties)}.select(&:toggled_on?)
-
-Limelight::Application.config.available_services = services.index_by(&:slug)
