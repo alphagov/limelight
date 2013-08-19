105c9747ff8b2498ae299a46f47500395441c47f
Oliver Spindler <os@oliverspindler.com>
2013-08-19 11:35:18 +0100

    Display additional data in failures table

--- app/assets/javascripts/vehicle-excise-duty/views/failures-table.js
+++ app/assets/javascripts/vehicle-excise-duty/views/failures-table.js
@@ -18,6 +18,36 @@ function (Table) {
         getValue: function (model) {
           return this.formatNumericLabel(model.get('count'));
         }
+      },
+      {
+        id: 'fraction',
+        title: 'Percentage of total errors',
+        sortable: true,
+        defaultDescending: true,
+        getValue: function (model) {
+          return this.formatPercentage(model.get('fraction'));
+        }
+      },
+      {
+        id: 'change',
+        title: 'Difference from week before',
+        sortable: true,
+        defaultDescending: true,
+        getValue: function (model) {
+          var change = model.get('change');
+          if (change == null) {
+            return '-';
+          }
+
+          var displayValue = this.formatNumericLabel(Math.abs(change));
+          if (change > 0) {
+            return '<span class="increase">&plus;' + displayValue + '</span>';
+          } else if (change < 0) {
+            return '<span class="decrease">&minus;' + displayValue + '</span>';
+          } else {
+            return '0';
+          }
+        }
       }
     ],
     
