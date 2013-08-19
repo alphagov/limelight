a835ef26559939fbc38032c95f7d85b9e0cf2d88
Oliver Spindler <os@oliverspindler.com>
2013-08-19 15:53:56 +0100

    Do not show graphs on vehicle licensing pages in IE6-8

--- app/assets/javascripts/vehicle-licensing/controllers/vehicle-license-volumes-module.js
+++ app/assets/javascripts/vehicle-licensing/controllers/vehicle-license-volumes-module.js
@@ -4,35 +4,41 @@ define([
   'extensions/views/tabs',
   'extensions/views/graph/headline',
 ],
-  function (ServicesCollection, TimeseriesGraph, Tabs, Headline) {
-    return function (selector, id, type) {
-      var serviceCollection = new ServicesCollection([], {
-        seriesList: [{ id: id, title: type }]
-      });
-      serviceCollection.query.set('period', 'week');
-      serviceCollection.fetch();
+function (ServicesCollection, TimeseriesGraph, Tabs, Headline) {
+  return function (selector, id, type) {
 
-      new TimeseriesGraph({
-        el: $(selector),
-        collection: serviceCollection
-      });
+    if ($('.lte-ie8').length) {
+      // do not attempt to show graphs in legacy IE
+      return;
+    }
 
-      var tabs = new Tabs({
-        el: $(selector + '-nav'),
-        model: serviceCollection.query,
-        attr: 'period',
-        tabs: [
-          {id: "month", name: "Monthly"},
-          {id: "week", name: "Weekly"}
-        ]
-      });
-      tabs.render();
+    var serviceCollection = new ServicesCollection([], {
+      seriesList: [{ id: id, title: type }]
+    });
+    serviceCollection.query.set('period', 'week');
+    serviceCollection.fetch();
 
-      var headline = new Headline({
-        el: $(selector + '-headline'),
-        model: serviceCollection.query,
-        prefix: 'Applications submissions'
-      });
-      headline.render();
-    };
-  });
+    new TimeseriesGraph({
+      el: $(selector),
+      collection: serviceCollection
+    });
+
+    var tabs = new Tabs({
+      el: $(selector + '-nav'),
+      model: serviceCollection.query,
+      attr: 'period',
+      tabs: [
+        {id: "month", name: "Monthly"},
+        {id: "week", name: "Weekly"}
+      ]
+    });
+    tabs.render();
+
+    var headline = new Headline({
+      el: $(selector + '-headline'),
+      model: serviceCollection.query,
+      prefix: 'Applications submissions'
+    });
+    headline.render();
+  };
+});
