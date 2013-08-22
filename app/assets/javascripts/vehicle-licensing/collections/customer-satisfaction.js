2153fe36bb3e78e582aadb929790486a39719d01
Paulo Schneider <paulo.schneider@gmail.com>
2013-08-22 12:51:00 +0100

    Collection for customer satisfaction

---
+++ app/assets/javascripts/vehicle-licensing/collections/customer-satisfaction.js
@@ -0,0 +1,24 @@
+define([
+  'extensions/collections/collection'
+],
+function (Collection) {
+  var maxScore = 5, minScore = 1;
+
+  var scoreToPercentage = function(score) {
+    return (maxScore - score) / (maxScore - minScore);
+  }
+
+  var CustomerSatisfaction = Collection.extend({
+
+    parse: function(response) {
+      return _.map(response.data, function(entry) {
+        return _.extend(entry, {
+          satisfaction_sorn: scoreToPercentage(entry.satisfaction_sorn),
+          satisfaction_tax_disc: scoreToPercentage(entry.satisfaction_tax_disc)
+        });
+      });
+    }
+  });
+
+  return CustomerSatisfaction;
+});
