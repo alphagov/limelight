9275aa4893880c0369c0c00ec6c1afe65ce8f235
Oliver Spindler <os@oliverspindler.com>
2013-08-30 16:45:52 +0100

    Switch date formatting for digital takeup number view

--- app/assets/javascripts/vehicle-licensing/views/digital-takeup-number.js
+++ app/assets/javascripts/vehicle-licensing/views/digital-takeup-number.js
@@ -11,11 +11,11 @@ function (SingleStatView) {
     },
 
     getLabel: function () {
-      var numWeeks = this.collection.at(0).get('values').length;
+      var numPeriods = this.collection.at(0).get('values').length;
       return [
         'last',
-        numWeeks,
-        this.pluralise('week', numWeeks)
+        numPeriods,
+        this.pluralise(this.collection.query.get('period'), numPeriods)
       ].join(' ');
     },
 
@@ -25,12 +25,7 @@ function (SingleStatView) {
 
     getLabelSelected: function (selection) {
       var model = selection.selectedModel;
-      var start = model.get('_start_at');
-      var end = moment(model.get('_end_at')).subtract(1, 'days');
-
-      var startLabel = start.format(start.month() === end.month() ? 'D' : 'D MMM');
-      var endLabel = end.format('D MMM YYYY');
-      return startLabel + ' to ' + endLabel;
+      return this.formatPeriod(selection.selectedModel, 'month');
     }
   });
 
